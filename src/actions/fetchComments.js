2ad3c577579f4e2b9be178643b15f15e5625748a
Eric Tai Chi Loong <ericloong90@gmail.com>
2018-12-05 18:08:36 +0800

    Testing axios

--- src/actions/fetchComments.js
+++ src/actions/fetchComments.js
@@ -1,9 +1,15 @@
+import axios from 'axios'
 import { FETCH_COMMENTS } from 'actions/types'
 
 export default () => {
-  let payload = fetch('http://jsonplaceholder.typicode.com/comments').then((response) => {
-    return response.json()
+  // let payload = fetch('http://jsonplaceholder.typicode.com/comments').then((response) => {
+  //   return response.json()
+  // })
+
+  let payload = axios.get('http://jsonplaceholder.typicode.com/comments').then((response) => {
+    return response.data
   })
+
 
   return {
     type: FETCH_COMMENTS,
